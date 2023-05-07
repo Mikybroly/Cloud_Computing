69427d9245d99232b44d266a211b4c4b9ce66f80
Migro <66359516+Mikybroly@users.noreply.github.com>
2023-05-07 20:53:55 +0200

    Add files via upload

---
+++ src/auth/signup.php
@@ -0,0 +1,19 @@
+<?php
+    require_once "../config/db_connection.php";
+    require_once "../function/search_user.php";
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $password_hash=password_hash($password, PASSWORD_BCRYPT);
+    var_dump($password_hash);
+
+    if(searchUser($email,$password,$conn)){
+        header("location: ../signup.html");
+    }else{
+        $query = "INSERT INTO user (email, password) VALUES ('".$email."', '".$password_hash."')";
+        $result = $conn->query($query);
+        header("location: ../home.html");
+    }
+ 
+?>
